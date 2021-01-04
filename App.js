3d0a21dcf401908ab8aacee6de4c0b9d067907be
sasideep <sasideep.bommareddy@k4connect.com>
2021-01-03 19:10:03 -0500

    upgrading notifications packages and android build

--- App.js
+++ App.js
@@ -7,34 +7,55 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, PushNotificationIOS } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  PushNotificationIOS,
+  Platform,
+} from 'react-native';
 import PushNotification from 'react-native-push-notification';
 import { StreamChat } from 'stream-chat';
-import { API_KEY, USER_TOKEN, USER_ID, SENDER_ID } from 'react-native-dotenv';
+import {
+  API_KEY,
+  SENDER_ID,
+  userStreamId,
+  userName,
+  userImage,
+} from 'react-native-dotenv';
+import messaging from '@react-native-firebase/messaging';
+
+PushNotification.configure({
+  onNotification(notification) {
+    console.log('********', notification);
+    notification.finish(PushNotificationIOS.FetchResult.NoData);
+  },
+  senderID: SENDER_ID,
+  requestPermissions: true,
+});
 
 export default class App extends Component {
   async componentDidMount() {
     const client = new StreamChat(API_KEY, null);
 
-    await client.setUser({ id: USER_ID }, USER_TOKEN);
+    const streamUserObj = {
+      id: userStreamId,
+      name: userName,
+      image: userImage,
+    };
 
-    PushNotification.configure({
-      onRegister(token) {
-        client
-          .addDevice(token.token, token.os === 'ios' ? 'apn' : 'firebase')
-          .then(() => {
-            console.log(`registered device with token ${token.token}`);
-          })
-          .catch((e) => {
-            console.error(`registering device failed: ${e}`);
-          });
-      },
-      onNotification(notification) {
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
-      },
-      senderID: SENDER_ID,
-      requestPermissions: true,
-    });
+    const token = await messaging().getToken();
+
+    await client.setUser(streamUserObj, userStreamId);
+
+    client
+      .addDevice(token, Platform.OS === 'ios' ? 'apn' : 'firebase')
+      .then(() => {
+        console.log(`beep : registered device with token`);
+      })
+      .catch((e) => {
+        console.error(`beep : registering device failed: ${e}`);
+      });
   }
 
   render() {
